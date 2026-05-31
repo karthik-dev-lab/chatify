9e8c22da293f27bcdbeb654499993254f4b3785d
karthik <karthiikk8348@gmail.com>
2026-05-31 10:02:12 +0530

    path changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,11 +22,12 @@ app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "frontend/build")));
-    app.get("*", (_, res) => {    
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
-    });
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
